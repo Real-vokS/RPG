d29d6e4cba737fd4564c16a6d350eba8ab9d85dc
Real-vokS <danielcskov@gmail.com>
2024-09-30 19:42:23 +0200

    Update main.go

--- main.go
+++ main.go
@@ -12,11 +12,16 @@ type Game struct {
 var player *Player
 
 func (g *Game) Update() error {
+
+	//Player Stuff
 	player.Move()
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+
+	//player stuff
 	player.Draw(screen)
 }
 
@@ -26,7 +31,9 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 
+	//player stuff
 	player = NewPlayer()
+
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Project A")
 	if err := ebiten.RunGame(&Game{}); err != nil {
