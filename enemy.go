fa16c879c7cd444c7a27a0a2c8b6a3388a79acab
Real-vokS <danielcskov@gmail.com>
2024-10-03 21:47:08 +0200

    Update enemy.go

--- enemy.go
+++ enemy.go
@@ -1,16 +1,34 @@
 package main
 
-type enemy struct {
+type Enemy struct {
+	speed float64
+	x float64
+	y float64
+	enemyImage *ebiten.Image
+	Health int
 }
 
-func (e *enemy) DropItem {
+func newEnemy() *Enemy {
+	enemyImg:= ebiten.NewImage(64,64) 				  // 64 x 64 pixels
+	enemyImg.Fill(color{R: 0, G: 255, B:255, A: 255}) //only test
+
+	return &Enemy{
+		speed: 1.5,
+		x: 200
+		y: 200
+		enemyImage: enemyImg,
+		Health: 20,
+	}
+}
+
+func (e *Enemy) DropItem {
 	
 }
 
-func (e *enemy) DrawEnemy {
+func (e *Enemy) DrawEnemy {
 
 }
 
-func (e *enemy) UpdateEnemy {
+func (e *Enemy) UpdateEnemy {
 
 }
